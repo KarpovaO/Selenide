abace5d1cf744df6246753d94a3a06b36e631a0f
KarpovaO <grose@yandex.ru>
2023-04-12 19:27:09 +0300

    доработка

--- src/test/java/DeliveryForCardTest.java
+++ src/test/java/DeliveryForCardTest.java
@@ -20,18 +20,14 @@ public class DeliveryForCardTest {
 
     @Test
     void shouldRegisterCardWithDelivery() {
-
-//        LocalDate meetingDate = LocalDate.now().plusDays(3);
-//        String meetingDateStr = meetingDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        open("http://localhost:9999");
 
         String planningDate = generateDate(3, "dd.MM.yyyy");
 
-        open("http://localhost:9999");
-
         $("[data-test-id= 'city'] input").setValue("Москва");
         $("div.menu div.menu-item span.menu-item__control").click();
-//        $("[placeholder='Дата встречи']").setValue("").setValue(planningDate);
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("td.calendar__day.calendar__day_state_current").click();
         $("[data-test-id= 'name'] input").setValue("Иванов Иван");
         $("[data-test-id= 'phone'] input").setValue("+79161112233");
